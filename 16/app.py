981beb35cada08110558c2fe4c8e59eca7f99814
laiananardi <laiana.nardi@gmail.com>
2025-02-05 10:00:56 -0300

    day 16 - customize theme

---
+++ 16/app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+# https://htmlcolorcodes.com/
+
+st.title('Customizing the theme of Streamlit apps')
+
+st.write('Contents of the `.streamlit/config.toml` file of this app')
+
+st.code("""
+[theme]
+primaryColor="#7d3c98"
+backgroundColor="#273746"
+secondaryBackgroundColor="#566573"
+textColor="#FFFFFF"
+font="monospace"
+""")
+
+number = st.sidebar.slider('Select a number:', 0, 10, 5)
+st.write('Selected number from slider widget is:', number)
