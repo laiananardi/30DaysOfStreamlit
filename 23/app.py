9f1948935d93cbad8db2cef7524f4663f050f055
laiananardi <laiana.nardi@gmail.com>
2025-02-12 21:14:42 -0300

    day 23 - query params

---
+++ 23/app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.title('st.query_params')
+
+with st.expander('About this app'):
+  st.write("`st.query_params` allows the retrieval of query parameters directly from the URL of the user's browser.")
+
+# 1. Instructions
+st.header('1. Instructions')
+st.markdown('''
+In the above URL bar of your internet browser, append the following:
+`?firstname=Jack&surname=Beanstalk`
+after the base URL `http://share.streamlit.io/dataprofessor/st.query_params/`
+such that it becomes 
+`http://share.streamlit.io/dataprofessor/st.query_params/?firstname=Jack&surname=Beanstalk`
+''')
+
+# 3. Retrieving and displaying information from the URL
+st.header('2. Retrieving and displaying information from the URL')
+
+firstname = st.query_params["firstname"]
+surname = st.query_params["surname"]
+
+st.write(f'Hello **{firstname} {surname}**, how are you?')
